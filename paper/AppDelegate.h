033fe7a5609abe7126fa1eedd4ab37f99a37321a
Gabriel Valdivia <valdivia.gabriel@gmail.com>
2014-06-28 14:26:25 -0700

    Initial Commit

---
+++ paper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  paper
+//
+//  Created by Gabriel Valdivia on 6/28/14.
+//  Copyright (c) 2014 Gabriel Valdivia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ paper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  paper
+//
+//  Created by Gabriel Valdivia on 6/28/14.
+//  Copyright (c) 2014 Gabriel Valdivia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
